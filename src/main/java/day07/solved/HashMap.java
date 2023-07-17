e96fdd6680c5eb2b1b7ba64316bfc68de48e3ba1
jaleela9502 <jaleelasheik123@gmail.com>
2023-07-17 21:50:50 +0530

    update day08

---
+++ src/main/java/day07/solved/HashMap.java
@@ -0,0 +1,28 @@
+package day07.solved;
+
+import java.util.Map;
+
+public class HashMap {
+
+	public static void main(String[] args) {
+		Map<String,Integer> countMap = Map<String, Integer> new HashMap;
+		String[] arrNames = {"Prathiusha","Naresh","Prathiusha","Vishali","Nithilan","Vishali","Prathiusha"};
+		
+		for(int i=0;i<arrNames.length;i++) {
+			
+			if() {
+				
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
